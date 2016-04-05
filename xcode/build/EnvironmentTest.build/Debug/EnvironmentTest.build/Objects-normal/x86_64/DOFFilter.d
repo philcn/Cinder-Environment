146b976ad159a98a1ea4a5f4f1994c27999de6d2
Phil Guo <phil@pku.edu.cn>
2016-04-04 23:34:09 -0400

    added gitignore

--- xcode/build/EnvironmentTest.build/Debug/EnvironmentTest.build/Objects-normal/x86_64/DOFFilter.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Phil/Projects/cinder_0.9.0/apps/EnvironmentTest/src/DOFFilter.cpp \
-  /Users/Phil/Projects/cinder_0.9.0/apps/EnvironmentTest/src/DOFFilter.h
--- xcode/build/EnvironmentTest.build/Debug/EnvironmentTest.build/Objects-normal/x86_64/EnvironmentTestApp.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/Phil/Projects/cinder_0.9.0/apps/EnvironmentTest/src/EnvironmentTestApp.cpp \
-  ../../../include/cinder/app/RendererGl.h \
-  ../../../include/cinder/CameraUi.h ../../../include/cinder/ObjLoader.h \
-  /Users/Phil/Projects/cinder_0.9.0/apps/EnvironmentTest/src/DOFFilter.h
